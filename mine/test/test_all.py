3a58db869efeef7fae6101ce1b6af4c4ac7ceec8
Jace Browning <jacebrowning@gmail.com>
2014-12-04 18:39:45 -0500

    Hard-code addresses in integration tests

--- mine/test/test_all.py
+++ mine/test/test_all.py
@@ -14,6 +14,48 @@ import yorm
 
 
 @pytest.mark.integration
+def test_settings():
+    """Verify a sample file is created."""
+    path = os.path.join(FILES, 'mine.yml')
+
+    if os.path.exists(path):
+        os.remove(path)
+
+    settings = Settings()
+    yorm.store(settings, path)
+
+    itunes = Application('itunes')
+    itunes.versions.mac = ''
+    itunes.versions.windows = 'iTunes.exe'
+
+    iphoto = Application('iphoto')
+    iphoto.versions.mac = 'iPhoto'
+
+    mac = Computer('macbook', 'Other.local')
+    mac2 = Computer('macbook-pro')
+
+    configuration = ProgramConfiguration()
+    configuration.applications = [itunes, iphoto]
+    configuration.computers = [mac, mac2]
+
+    settings.configuration = configuration
+
+    mac_state = State('macbook-pro')
+    mac_state.timestamps.started = 444
+
+    itunes_status = Status('itunes')
+    itunes_status.computers = [mac_state]
+
+    status = ProgramStatus()
+    status.applications = [itunes_status]
+    status.counter = 499
+
+    settings.status = status
+
+    assert os.path.exists(path)
+
+
+@pytest.mark.integration
 def test_settings_out():
     """Verify a sample file is created."""
     path = os.path.join(FILES, 'mine-out.yml')
@@ -31,11 +73,8 @@ def test_settings_out():
     iphoto = Application('iphoto')
     iphoto.versions.mac = 'iPhoto'
 
-    mac = Computer('macbook')
-    mac.hostname = 'Jaces-MacBook'
-    mac.address.external = '1.2.3.4'
-
-    mac2 = Computer('macbook-pro')
+    mac = Computer('macbook', 'Jaces-MacBook', '1.1.1.1', '2.2.2.2')
+    mac2 = Computer('macbook-pro', 'Jaces-MacBook-2', '3.3.3.3', '4.4.4.4')
 
     configuration = ProgramConfiguration()
     configuration.applications = [itunes, iphoto]

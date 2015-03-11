c0d9708b8cffe7b855e86f649852902c6bcddeec
Jace Browning <jacebrowning@gmail.com>
2015-03-11 00:52:43 -0400

    Prototype conflict deletion using glob

--- mine/services.py
+++ mine/services.py
@@ -1,6 +1,7 @@
 """Functions to interact with sharing services."""
 
 import os
+import glob
 import getpass
 
 from . import common
@@ -20,33 +21,43 @@ DEPTH = 3  # number of levels to search for the settings file
 log = common.logger(__name__)
 
 
-def get_path(top=None):
-    """Return the path to the settings file."""
-    log.info("looking for settings file...")
+def find_root(top=None):
+    """Get the root of the shared directory."""
     top = top or _default_top()
 
     log.debug("looking for sharing service in '%s'...", top)
     for directory in os.listdir(top):
         if directory in SERVICES:
-            service = os.path.join(top, directory)
-            log.debug("found sharing service: %s", service)
-            log.debug("looking for '%s' in '%s'...", FILENAME, service)
-            for dirpath, dirnames, _, in os.walk(service):
-                depth = dirpath.count(os.path.sep) - service.count(os.path.sep)
-                if depth >= DEPTH:
-                    del dirnames[:]
-                    continue
-                path = os.path.join(dirpath, FILENAME)
-                if os.path.isfile(path) and \
-                        not os.path.isfile(os.path.join(path, 'setup.py')):
-                    log.info("found settings file: %s", path)
-                    return path
+            path = os.path.join(top, directory)
+            log.debug("found sharing service: %s", path)
+            return path
+
+    raise EnvironmentError("no sharing service found")
+
+
+def find_config_path(top=None, root=None):
+    """Get the path to the settings file."""
+    log.info("looking for settings file...")
+    top = top or _default_top()
+    root = root or find_root(top=top)
+
+    log.debug("looking for '%s' in '%s'...", FILENAME, root)
+    for dirpath, dirnames, _, in os.walk(root):
+        depth = dirpath.count(os.path.sep) - root.count(os.path.sep)
+        if depth >= DEPTH:
+            del dirnames[:]
+            continue
+        path = os.path.join(dirpath, FILENAME)
+        if os.path.isfile(path) and \
+                not os.path.isfile(os.path.join(path, 'setup.py')):
+            log.info("found settings file: %s", path)
+            return path
 
     raise EnvironmentError("no '{}' file found".format(FILENAME))
 
 
 def _default_top():
-    """Return the default search path."""
+    """Get the default search path."""
     username = getpass.getuser()
     log.debug("looking for home directory...")
     for root in ROOTS:
@@ -56,3 +67,14 @@ def _default_top():
             return dirpath
 
     raise EnvironmentError("no home directory found for '{}'".format(username))
+
+
+def delete_conflicts(root=None, force=False):
+    """Delete all files with conflicted filenames."""
+    root = root or find_root()
+
+    log.info("%s conflicted files...", 'deleting' if force else 'displaying')
+    pattern = os.path.join(root, "*(*'s conflicted copy *-*-*)*")
+    log.debug("pattern: %r", pattern)
+    for path in glob.iglob(pattern):
+        print(path)
--- mine/test/test_services.py
+++ mine/test/test_services.py
@@ -15,7 +15,7 @@ def test_find_dropbox(tmpdir):
     tmpdir.chdir()
     _touch('Dropbox', 'mine.yml')
 
-    path = services.get_path(tmpdir.strpath)
+    path = services.find_config_path(tmpdir.strpath)
 
     assert os.path.isfile(path)
 
@@ -25,7 +25,7 @@ def test_find_dropbox_personal(tmpdir):
     tmpdir.chdir()
     _touch('Dropbox (Personal)', 'mine.yml')
 
-    path = services.get_path(tmpdir.strpath)
+    path = services.find_config_path(tmpdir.strpath)
 
     assert os.path.isfile(path)
 
@@ -37,20 +37,20 @@ def test_find_depth(tmpdir):
     _touch('Dropbox', 'a', 'b', 'mine.yml')
 
     with pytest.raises(OSError):
-        services.get_path(tmpdir.strpath)
+        services.find_config_path(tmpdir.strpath)
 
 
 @patch('os.path.isdir', Mock(return_value=False))
 def test_find_no_home():
     """Verify an error occurs when no home directory is found."""
     with pytest.raises(EnvironmentError):
-        services.get_path()
+        services.find_config_path()
 
 
 def test_find_no_share():
     """Verify an error occurs when no service directory is found."""
     with pytest.raises(EnvironmentError):
-        services.get_path(FILES)
+        services.find_config_path(FILES)
 
 
 def _touch(*parts):
